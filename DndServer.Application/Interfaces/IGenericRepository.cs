08cfb711d94f2b1c48783cac4a8e6fed327657e9
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-04-05 20:29:39 +0700

    feat: создан GenericRepo и DataContext WIP

---
+++ DndServer.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DndServer.Application.Interfaces;
+
+public interface IGenericRepository<T>
+{
+    void Create(T item);
+    T? FindById(int id);
+    IEnumerable<T> Get();
+    IEnumerable<T> Get(Func<T, bool> predicate);
+    void Remove(T item);
+    void Update(T item);
+}
---
+++ DndServer.Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using DndServer.Domain.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace DndServer.Infrastructure.Persistence;
+
+public class DataContext : DbContext
+{
+    public DbSet<User> User => Set<User>();
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+}
