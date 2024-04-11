8f5be5ac125779eaf4ad6cce7376b6ff5ebfc6a1
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-04-11 18:22:13 +0700

    feat: репозитории добавлены в DI

--- DndServer.Infrastructure/ConfigureServices.cs
+++ DndServer.Infrastructure/ConfigureServices.cs
@@ -1,4 +1,26 @@
-﻿using DndServer.Infrastructure.Persistence;
+﻿using DndServer.Application.Interfaces.Character;
+using DndServer.Application.Interfaces.Character.Background;
+using DndServer.Application.Interfaces.Character.Class;
+using DndServer.Application.Interfaces.Character.Condition;
+using DndServer.Application.Interfaces.Character.Inventory;
+using DndServer.Application.Interfaces.Character.Note;
+using DndServer.Application.Interfaces.Character.Race;
+using DndServer.Application.Interfaces.Character.Skill;
+using DndServer.Application.Interfaces.Character.Spell;
+using DndServer.Application.Interfaces.User;
+using DndServer.Application.Interfaces.World;
+using DndServer.Infrastructure.Persistence;
+using DndServer.Infrastructure.Persistence.Repositories.Character;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Background;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Class;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Condition;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Inventory;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Note;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Race;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Skill;
+using DndServer.Infrastructure.Persistence.Repositories.Character.Spell;
+using DndServer.Infrastructure.Persistence.Repositories.User;
+using DndServer.Infrastructure.Persistence.Repositories.World;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +39,26 @@ public static class ConfigureServices
             );
         Console.WriteLine(connectionString);
 
-        //services.AddScoped<IProjectRepository, ProjectRepository>();
+        services.AddScoped<IWorldRepository, WorldRepository>();
+        services.AddScoped<IWorldLinksRepository, WorldLinksRepository>();
+        services.AddScoped<IWikiRepository, WikiRepository>();
+        services.AddScoped<IWikiPageRepository, WikiPageRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ICharacterRepository, CharacterRepository>();
+        services.AddScoped<IBackgroundInstanceRepository, BackgroundInstanceRepository>();
+        services.AddScoped<IBackgroundTemplateRepository, BackgroundTemplateRepository>();
+        services.AddScoped<IClassInstanceRepository, ClassInstanceRepository>();
+        services.AddScoped<IClassTemplateRepository, ClassTemplateRepository>();
+        services.AddScoped<IConditionsRepository, ConditionsRepository>();
+        services.AddScoped<IObjectInstanceRepository, ObjectInstanceRepository>();
+        services.AddScoped<IObjectTemplateRepository, ObjectTemplateRepository>();
+        services.AddScoped<INoteRepository, NoteRepository>();
+        services.AddScoped<IRaceInstanceRepository, RaceInstanceRepository>();
+        services.AddScoped<IRaceTemplateRepository, RaceTemplateRepository>();
+        services.AddScoped<ISkillInstanceRepository, SkillInstanceRepository>();
+        services.AddScoped<ISkillTemplateRepository, SkillTemplateRepository>();
+        services.AddScoped<ISpellInstanceRepository, SpellInstanceRepository>();
+        services.AddScoped<ISpellTemplateRepository, SpellTemplateRepository>();
 
         return services;
     }
