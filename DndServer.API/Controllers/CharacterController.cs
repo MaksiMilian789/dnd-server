cb43668c12688bc8660b4df14f3010d2e5bd7edf
MaksiMilian789 <maksim.kozyrin789@mail.ru>
2024-04-30 18:35:49 +0700

    feat: создан констроллер workshop

--- DndServer.API/Controllers/CharacterController.cs
+++ DndServer.API/Controllers/CharacterController.cs
@@ -12,20 +12,10 @@ namespace DndServer.API.Controllers;
 public class CharacterController : ControllerBase
 {
     private readonly ICharacterService _characterService;
-    private readonly IBackgroundService _backgroundService;
-    private readonly IRaceService _raceService;
-    private readonly IClassService _classService;
-    private readonly ISkillService _skillService;
 
-    public CharacterController(
-        ICharacterService characterService, IBackgroundService backgroundService, IRaceService raceService,
-        IClassService classService, ISkillService skillService)
+    public CharacterController(ICharacterService characterService)
     {
         _characterService = characterService;
-        _backgroundService = backgroundService;
-        _raceService = raceService;
-        _classService = classService;
-        _skillService = skillService;
     }
 
     /// <summary>
@@ -86,102 +76,5 @@ public class CharacterController : ControllerBase
     public async Task AddSkill(int id, int skillId)
     {
         await _characterService.AddSkill(id, skillId);
-    }
-
-    /// <summary>
-    ///     Список шаблонов классов
-    /// </summary>
-    [HttpGet("getClasses")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task<ActionResult<List<ClassDto>>> GetClasses()
-    {
-        return await _classService.GetClasses();
-    }
-
-    /// <summary>
-    ///     Создание шаблона класса
-    /// </summary>
-    [HttpPost("class")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task CreateClass([FromBody] ClassCreateDto classs)
-    {
-        await _classService.CreateClassTemplate(classs);
-    }
-
-    /// <summary>
-    ///     Список шаблонов рас
-    /// </summary>
-    [HttpGet("getRaces")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task<ActionResult<List<RaceDto>>> GetRaces()
-    {
-        return await _raceService.GetRaces();
-    }
-
-    /// <summary>
-    ///     Создание шаблона расы
-    /// </summary>
-    [HttpPost("race")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task CreateRace([FromBody] RaceCreateDto race)
-    {
-        await _raceService.CreateRaceTemplate(race);
-    }
-
-    /// <summary>
-    ///     Список шаблонов предысторий
-    /// </summary>
-    [HttpGet("getBackgrounds")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task<ActionResult<List<BackgroundDto>>> GetBackgrounds()
-    {
-        return await _backgroundService.GetBackgrounds();
-    }
-
-    /// <summary>
-    ///     Создание шаблона предыстории
-    /// </summary>
-    [HttpPost("background")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task CreateBackground([FromBody] BackgroundCreateDto background)
-    {
-        await _backgroundService.CreateBackgroundTemplate(background);
-    }
-
-
-    /// <summary>
-    ///     Список шаблонов skill
-    /// </summary>
-    [HttpGet("getSkills")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task<ActionResult<List<SkillDto>>> GetSkills()
-    {
-        return await _skillService.GetSkills();
-    }
-
-    /// <summary>
-    ///     Создание шаблона skill
-    /// </summary>
-    [HttpPost("skill")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesDefaultResponseType]
-    public async Task CreateSkill([FromBody] SkillCreateDto skill)
-    {
-        await _skillService.CreateSkillTemplate(skill);
     }
 }
---
+++ DndServer.API/Controllers/WorkshopController.cs
@@ -0,0 +1,150 @@
+﻿using DndServer.Application.Characters.Interfaces;
+using DndServer.Application.Characters.Models;
+using DndServer.Application.Characters.Models.Create;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DndServer.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class WorkshopController : ControllerBase
+{
+    private readonly IBackgroundService _backgroundService;
+    private readonly IRaceService _raceService;
+    private readonly IClassService _classService;
+    private readonly ISkillService _skillService;
+    private readonly IConditionService _conditionService;
+
+    public WorkshopController(IBackgroundService backgroundService, IRaceService raceService,
+        IClassService classService, ISkillService skillService, IConditionService conditionService)
+    {
+        _backgroundService = backgroundService;
+        _raceService = raceService;
+        _classService = classService;
+        _skillService = skillService;
+        _conditionService = conditionService;
+    }
+
+    /// <summary>
+    ///     Список шаблонов состояний
+    /// </summary>
+    [HttpGet("getConditions")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<ConditionDto>>> GetConditions()
+    {
+        return await _conditionService.GetConditions();
+    }
+
+    /// <summary>
+    ///     Создание шаблона состояния
+    /// </summary>
+    [HttpPost("condition")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task CreateCondition([FromBody] ConditionCreateDto condition)
+    {
+        await _conditionService.CreateCondition(condition);
+    }
+
+    /// <summary>
+    ///     Список шаблонов классов
+    /// </summary>
+    [HttpGet("getClasses")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<ClassDto>>> GetClasses()
+    {
+        return await _classService.GetClasses();
+    }
+
+    /// <summary>
+    ///     Создание шаблона класса
+    /// </summary>
+    [HttpPost("class")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task CreateClass([FromBody] ClassCreateDto classs)
+    {
+        await _classService.CreateClassTemplate(classs);
+    }
+
+    /// <summary>
+    ///     Список шаблонов рас
+    /// </summary>
+    [HttpGet("getRaces")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<RaceDto>>> GetRaces()
+    {
+        return await _raceService.GetRaces();
+    }
+
+    /// <summary>
+    ///     Создание шаблона расы
+    /// </summary>
+    [HttpPost("race")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task CreateRace([FromBody] RaceCreateDto race)
+    {
+        await _raceService.CreateRaceTemplate(race);
+    }
+
+    /// <summary>
+    ///     Список шаблонов предысторий
+    /// </summary>
+    [HttpGet("getBackgrounds")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<BackgroundDto>>> GetBackgrounds()
+    {
+        return await _backgroundService.GetBackgrounds();
+    }
+
+    /// <summary>
+    ///     Создание шаблона предыстории
+    /// </summary>
+    [HttpPost("background")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task CreateBackground([FromBody] BackgroundCreateDto background)
+    {
+        await _backgroundService.CreateBackgroundTemplate(background);
+    }
+
+
+    /// <summary>
+    ///     Список шаблонов skill
+    /// </summary>
+    [HttpGet("getSkills")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult<List<SkillDto>>> GetSkills()
+    {
+        return await _skillService.GetSkills();
+    }
+
+    /// <summary>
+    ///     Создание шаблона skill
+    /// </summary>
+    [HttpPost("skill")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesDefaultResponseType]
+    public async Task CreateSkill([FromBody] SkillCreateDto skill)
+    {
+        await _skillService.CreateSkillTemplate(skill);
+    }
+}
